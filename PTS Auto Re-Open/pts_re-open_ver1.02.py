2a1c576c0864b31b08aa5eca8ed8b7f12ec15bfe
Napat Sri-ampun <Napat.Sri-Ampun@wdc.com>
2020-03-03 10:18:55 +0700

    Fix some code

--- PTS Auto Re-Open/pts_re-open_ver1.02.py
+++ PTS Auto Re-Open/pts_re-open_ver1.02.py
@@ -5,20 +5,8 @@ import os
 import urllib.request
 from os import path
 
-try:
-    drives = os.listdir('z:\\')
-    print(drives)
-except Exception as e:
-    logging.error(str(e))
 
-def connect(host=r'http://google.com'):
-    try:
-        urllib.request.urlopen(host) #Python 3.x
-        return True
-    except:
-        return False
-
-while True:
+while True:        
     process_ls = []
     for proc in psutil.process_iter():
         try:
@@ -33,29 +21,18 @@ while True:
 
     try:
         chk_process = next(item for item in process_ls if item["name"] == "notepad++.exe")
-
-        #print(chk_process)
-        #print(type(chk_process))
-        print(chk_process['name'])
-    
     except Exception as e:
         #os.startfile(r'C:\Program Files\Notepad++\notepad++.exe')
         logging.error(str(e))
         print('Re-open program')
         
-    
-    if connect():
-        print("Internet Connected")
-        
         try:
-            drives = os.listdir('t:\\')
+            drives = os.listdir('z:\\')
             #print(drives)
         except Exception as e:
             logging.error(str(e))
-            print("Line Send -> Connot connect to MapDrive.")
+            
         
-    else:
-        print("Line Send -> Cannot connect the internet.")
     time.sleep(1)
     break
  
