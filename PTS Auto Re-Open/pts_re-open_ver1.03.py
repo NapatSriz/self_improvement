de385f063e3e744c19b03301dbdfd4912acbeb21
Napat Sri-ampun <Napat.Sri-Ampun@wdc.com>
2020-03-03 10:26:03 +0700

    Change paremeter for testing on Shimada machine.

--- PTS Auto Re-Open/pts_re-open_ver1.03.py
+++ PTS Auto Re-Open/pts_re-open_ver1.03.py
@@ -4,6 +4,7 @@ import logging
 import os
 import urllib.request
 from os import path
+import mysql.connector
 
 
 while True:        
@@ -22,17 +23,28 @@ while True:
     try:
         chk_process = next(item for item in process_ls if item["name"] == "notepad++.exe")
     except Exception as e:
-        #os.startfile(r'C:\Program Files\Notepad++\notepad++.exe')
+        os.startfile(r'C:\stock_part\PG\Convert_file_Cleaner.exe')
         logging.error(str(e))
-        print('Re-open program')
+        print('Open program')
         
         try:
-            drives = os.listdir('z:\\')
+            drives = os.listdir('J:\\')
             #print(drives)
+            mydb = mysql.connector.connect(
+            host="10.144.134.163",
+            database="cleaner_log",
+            user="AK800",
+            passwd="zaq12wsx"
+
+)
+                
         except Exception as e:
             logging.error(str(e))
+            for proc in psutil.process_iter(attrs=['pid', 'name']):
+                if 'Convert_file_Cleaner' in proc.info['name']:
+                    proc.kill()
+            os.startfile(r'C:\stock_part\PG\Convert_file_Cleaner.exe')
             
         
     time.sleep(1)
-    break
- 
+    #break
