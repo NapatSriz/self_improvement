0d093c9a1a36b5df548fa5ad0d90000cbd41a048
Napat Sri-ampun <Napat.Sri-Ampun@wdc.com>
2020-03-13 06:23:56 +0700

    add 1st file to count float defects

--- PTS Auto Re-Open/pts_re-open_ver1.05.py
+++ PTS Auto Re-Open/pts_re-open_ver1.05.py
@@ -10,14 +10,14 @@ import subprocess
 
 
 
-sys.exit()
+
 
 while True:        
     process_ls = []
     for proc in psutil.process_iter(attrs=['pid', 'name']):
         #print(proc.name())
         process_ls.append(proc.name())
-        if 'notepad++' in proc.info['name']:
+        if 'Convert_file_Cleaner' in proc.info['name']:
             try:
                 mydb = mysql.connector.connect(
                 host="10.144.134.163",
@@ -31,9 +31,9 @@ while True:
                 logging.error(str(e))
                 print("Cannot connect to DATABASE")
                 proc.kill()
-                os.startfile(r'C:\Program Files\Notepad++\notepad++.exe')
-    if not 'notepad++.exe' in process_ls:
-        os.startfile(r'C:\Program Files\Notepad++\notepad++.exe')
+                os.startfile(r'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\Convert_file_Cleaner.exe')
+    if not 'Convert_file_Cleaner.exe' in process_ls:
+        os.startfile(r'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\Convert_file_Cleaner.exe')
         print("Program Re-Open")
     else:
         print("Program Openning")
